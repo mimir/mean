f4ef6dcc50b586b56e961b45ba566549e4e75b72
Johannes Griebler <j.griebler@stud.uni-goettingen.de>
2026-09-25 15:33:03 +0200

    Add an example of a bad substitution

--- Examples.lean
+++ Examples.lean
@@ -40,3 +40,51 @@ def comp' : Computation := ⟨factorial', expr([5] ((5, [4]).0, 1))⟩
 -- This example requires native_decide, both because it uses the free variable
 -- algorithm and because substitution is defined using well-founded recursion.
 example : comp.subst 0 expr((5, [4])) = comp' := by native_decide
+
+-- The following example is from Section 3.1.3 of the thesis.
+
+def illformed : Program := prog
+  λ int → int. [1] var[0] + [2] ()
+  λ int → int. var[0] + [2] ()
+  λ [] → int. var[0] + var[1]
+
+def ex : Expr := expr([1] var[0] + [2] ())
+
+def vm : VarMap 3 := .mk _ 0 expr(0)
+
+def fm : FunMap 3 := .mk _
+
+example : ⊢ illformed := by decide
+
+example : illformed ⊢ ex : ty(int) := by decide
+
+example : ¬illformed.WF := by native_decide
+
+def wellformed : Program := illformed.setBody 0 expr(42)
+
+example : wellformed.WF := by native_decide
+
+def illformed' : Program := prog
+  λ int → int. [1] var[0] + [2] ()
+  λ int → int. var[0] + [2] ()
+  λ [] → int. var[0] + var[1]
+  λ int → int. 0 + [4] ()
+  λ [] → int. 0 + var[3]
+
+def wellformed' : Program := illformed'.setBody 0 expr(42)
+
+def illformedResult : SubstResult illformed ex vm fm := ex.subst illformed vm fm
+
+def wellformedResult : SubstResult wellformed ex vm fm := ex.subst wellformed vm fm
+
+example : illformedResult.expr = expr([3] 0 + [4] ()) := by native_decide
+
+example : illformedResult.program = illformed' := by native_decide
+
+example : illformedResult.expr.Free illformed' 3 := by native_decide
+
+example : wellformedResult.expr = expr([3] 0 + [4] ()) := by native_decide
+
+example : wellformedResult.program = wellformed' := by native_decide
+
+example : wellformedResult.expr.Free wellformed' 3 := by native_decide
